2710e53cdf71d6bd01d598f186dbc071c6fbc080
MrDeys <115146793+MrDeys@users.noreply.github.com>
2025-10-21 14:29:56 +0300

    Task2

---
+++ Task2/Task2/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Task2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.numericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBar = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.Color.LightBlue;
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(794, 338);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox_Paint);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.numericUpDown);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.trackBar);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 347);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(794, 100);
+            this.panel1.TabIndex = 1;
+            // 
+            // numericUpDown
+            // 
+            this.numericUpDown.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.numericUpDown.Cursor = System.Windows.Forms.Cursors.Default;
+            this.numericUpDown.Location = new System.Drawing.Point(615, 52);
+            this.numericUpDown.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown.Name = "numericUpDown";
+            this.numericUpDown.Size = new System.Drawing.Size(130, 22);
+            this.numericUpDown.TabIndex = 5;
+            this.numericUpDown.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(611, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Количество точек";
+            // 
+            // trackBar
+            // 
+            this.trackBar.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.trackBar.AutoSize = false;
+            this.trackBar.Cursor = System.Windows.Forms.Cursors.Default;
+            this.trackBar.Location = new System.Drawing.Point(448, 45);
+            this.trackBar.Name = "trackBar";
+            this.trackBar.Size = new System.Drawing.Size(130, 45);
+            this.trackBar.TabIndex = 3;
+            this.trackBar.Value = 5;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(445, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Сглаживание";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button2.Location = new System.Drawing.Point(235, 14);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 60);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Добавить неровности";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button1.Location = new System.Drawing.Point(49, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Сгенерировать горизонт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 76.44444F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 23.55556F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
+
---
+++ Task2/Task2/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task2
+{
+    public partial class Form1 : Form
+    {
+        private List<PointF> mountainPoints;
+        private Random random = new Random();
+        private int currentPos = 0;
+        private int maxPos = 5;
+        private float smoothing = 0.5f;
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox.Paint += new PaintEventHandler(pictureBox_Paint);
+            GeneratePoints();
+        }
+
+        private void GeneratePoints()
+        {
+            mountainPoints = new List<PointF>
+            {
+                new PointF(0, pictureBox.Height / 2),
+                new PointF(pictureBox.Width, pictureBox.Height / 2)
+            };
+            currentPos = 0;
+        }
+
+        private void NextStep()
+        {
+            if (currentPos >= maxPos) return;
+
+            float distanceRange = (pictureBox.Height / 2.0f) * (float)Math.Pow(2, -currentPos * smoothing);
+
+            for (int i = 0; i < mountainPoints.Count - 1; i += 2)
+            {
+                PointF p1 = mountainPoints[i];
+                PointF p2 = mountainPoints[i + 1];
+
+                PointF midPoint = new PointF((p1.X + p2.X) / 2, (p1.Y + p2.Y) / 2);
+
+                float distance = (float)(random.NextDouble() * 2 - 1) * distanceRange;
+                midPoint.Y += distance;
+
+                midPoint.Y = Math.Max(0, midPoint.Y);
+                midPoint.Y = Math.Min(pictureBox.Height, midPoint.Y);
+
+                mountainPoints.Insert(i + 1, midPoint);
+            }
+            currentPos++;
+            pictureBox.Invalidate();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            maxPos = (int)numericUpDown.Value;
+            smoothing = trackBar.Value / 10.0f;
+
+            GeneratePoints();
+            pictureBox.Invalidate();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            NextStep();
+        }
+
+        private void pictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            using (GraphicsPath path = new GraphicsPath())
+            {
+                path.AddPolygon(GetPoints());
+
+                using (SolidBrush brush = new SolidBrush(Color.DarkSlateGray))
+                {
+                    e.Graphics.FillPath(brush, path);
+                }
+
+                using (Pen pen = new Pen(Color.Black, 2))
+                {
+                    e.Graphics.DrawLines(pen, mountainPoints.ToArray());
+                }
+            }
+        }
+
+        private PointF[] GetPoints()
+        {
+            List<PointF> Points = new List<PointF>(mountainPoints)
+            {
+                new PointF(pictureBox.Width, pictureBox.Height),
+                new PointF(0, pictureBox.Height)
+            };
+            return Points.ToArray();
+        }
+    }
+}
